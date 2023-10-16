c36637f5881183ec46236a2c0fe8c08f51354a16
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-10-15 22:38:27 -0300

    70% done

--- src/Complex.jsx
+++ src/Complex.jsx
@@ -2,21 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './Firebase';
 import { collection, query, onSnapshot } from 'firebase/firestore';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-
 
 export default function Complex() {
   const [complexData, setComplexData] = useState([]);
@@ -26,89 +19,54 @@ export default function Complex() {
     onSnapshot(q, (data) => {
       const finaldata = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       setComplexData(finaldata);
-       });
+    });
   }, []);
 
-  const [expandedCard, setExpandedCard] = useState(null);
-
-  const handleExpandClick = (cardId) => {
-    setExpandedCard(cardId === expandedCard ? null : cardId);
-  };
-
   return (
-    <div>
-      
-      <Grid container spacing={2} justifyContent="center" alignItems="center" margin="2%">
-        {complexData.map((complex) => {
-          let prop={
-            name:complex.name
-          };
-        return  <Grid item xs={4} key={complex.id}>
-            <Card sx={{ maxWidth: 400 , maxHeight: 700 }}>
-              <CardHeader
-                
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title={complex.name}
-                subheader={complex.location}
-              />
-              <CardMedia
-                component="img"
-                height="300"
-                image={complex.photo}
-                alt={complex.name}
-              />
-              <CardContent>
+    <div className='call'>
+      {complexData.map((complex) => {
+        let prop = {
+          name: complex.name,
+          photoo: complex.photoo,
+          photo: complex.photo,
+          phone: complex.phone,
+          email: complex.email,
+          feat: complex.feat,
+          description: complex.description,
+          Hours: complex.Hours,
+          location: complex.location,
+        };
+
+        return (
+          <Grid container key={complex.id} justifyContent="right" alignItems="right" margin="2%">
+            <Grid item xs={8}>
+              <div style={{ textAlign: 'right' , margin:'2%' }}>
+                <Typography variant="h5" component="div">
+                  {complex.name}
+                </Typography>
                 <Typography variant="body2" color="text.secondary" className='box'>
                   {complex.description}
                 </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-                <IconButton
-                  onClick={() => handleExpandClick(complex.id)}
-                  aria-expanded={expandedCard === complex.id}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </IconButton>
-              </CardActions>
-              <Collapse in={expandedCard === complex.id} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography paragraph>contact</Typography>
-                  <Typography paragraph className='infoo'>
-                   {complex.phone}
-                   {complex.email}
-                   {complex.location}
-                  </Typography>
-                  
-                </CardContent>
-              </Collapse>
-            </Card>
-            <CardActions>
-              <Link to={`/Deatils`} state={prop}>
-              <IconButton
-                aria-label="View Details"
-                color="primary"
-              >
-                View Details
-              </IconButton>
-              
-              </Link>
-          
-            </CardActions>
+                <Link to={`/Deatils`} state={prop}>
+                  <Button variant="contained" color="primary" style={{ margin:'1%' }}>
+                    View Details
+                  </Button>
+                </Link>
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <Card sx={{ width: 350 }}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={complex.photo}
+                  alt={complex.name}
+                />
+              </Card>
+            </Grid>
           </Grid>
-})}
-      </Grid>
-
+        );
+      })}
     </div>
   );
 }
--- src/Deatils.jsx
+++ src/Deatils.jsx
@@ -1,14 +1,68 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-export default function Deatils() {
-    const Location=useLocation();
-    const prop =Location.state;
+export default function Details() {
+  const location = useLocation();
+  const prop = location.state;
+
+  const hoursOptions = prop.Hours || [];
+  const days = ['on site', 'on site', 'on site', 'on site', 'on site', 'not on site', 'on site'];
 
   return (
-    
     <div>
-      {prop.name}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <Card>
+              <Card.Body>
+                <h1>{prop.name}</h1>
+                <p>معلومات إضافية:</p>
+                <p>{prop.phone}</p>
+                <p>{prop.email}</p>
+                <p>{prop.location}</p>
+
+                <Dropdown>
+                  <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                    اوقات العمل
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {hoursOptions.map((Hours, index) => (
+                      <Dropdown.Item key={index}>
+                        {days[index]} - {Hours}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-8">
+            <img
+              className="d-block w-100"
+              src={prop.photo}
+              alt="main"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <Carousel>
+              {prop.photoo.map((photoo, index) => (
+                <Carousel.Item key={index}>
+                  <img
+                    className="d-block w-100"
+                    src={photoo}
+                    alt={`Slide ${index}`}
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
