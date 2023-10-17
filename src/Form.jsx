4240624cc5beb70b30e1e68f1064f93f09969a09
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-10-16 21:59:51 -0300

    update 81%

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,7 +7,7 @@ const MyForm = () => {
         <h1>Contact Us</h1>
         <p>Have a question or need assistance? Feel free to reach out to us.</p>
         <form
-          action="https://formspree.io/brono.fatima20@gmail.com" // Replace with your Formspree email
+          action="https://formspree.io/f/xaygaaqk" // Replace with your Formspree email
           method="POST"
         >
           <div className="form-group">
