3483744e5de694f1c4348470d65f8de2a772d57d
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-10-20 17:00:55 -0300

    finaly done

--- src/compontes/About.jsx
+++ src/compontes/About.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+
 import "../App.css";
 import slide from "./11.jpeg";
 import Footer from '../Footer';
-import Top3 from './Top3';
-
+import React, { useState } from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button, Typography, Grid } from '@mui/material';
 export default function About() {
+  const items = [
+    {
+      title: 'خدمات سريعة',
+      content: '  يمكن للمستخدمين تقديم تقييماتهم وآرائهم حول المجمعات التي زاروها. يساعد ذلك المستخدمين الآخرين على اتخاذ قرارات مستنيرة عند اختيار مكان السكن المستخدم راح يكدر يتواصصل بشكل سريع وبه فريق المبيعات الخاص بكل مجمع ب الاضافة يكدر من خلالنا نكون حلقة تواصل ويه المجمع ',
+      logo: require('./undraw_World_re_768g.png'), 
+    },
+    {
+      title: 'معلومات مباشرة',
+      content: '  المستخدم راح يكدر يعرف معلومات عن المجمعات بسهولة مثل الموقع وخطط الشقق   يوفر الموقع معلومات مفصلة حول كل مجمع سكني. يمكنك العثور على معلومات حول الموقع وأوقات العمل وعدد البنايات والخدمات المتاحه والموقع واراء المستخدمين  ',
+      logo: require('./undraw_World_re_768g.png'), 
+    },
+    {
+      title: 'فريق مبيعات متواجد 24',
+      content: 'تكدرون تتواصلون ويه فريق المبيعات 24 ساعه حيكونون متوفرين بالاضاقة تكدرون تطون افتراحات باضافة المجماعات الجديدة واي خدمات تحبوها ',
+      logo: require('./undraw_World_re_768g.png'), 
+    },
+  ];
+  
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
+  };
   return (<>
     <div className="container">
       <div className="row">
@@ -25,7 +53,27 @@ export default function About() {
           <img src={slide} alt="img" width="100%" />
         </div>
       </div>
-      <Top3 />
+         </div>
+         <div className='caro'>
+    <div   style={{alignItems:'center',justifyContent:'center',textAlign:'center',marginTop:'5%'}}>
+    
+      <Carousel autoPlay={true} animation="slide" index={activeIndex} timeout={5000} style={{alignItems:'center',justifyContent:'center',textAlign:'center'}}>
+        {items.map((item, index) => (
+          <Paper className="pap" key={index} > 
+            <h3>مميزات موقع كومبليكس</h3>
+            <Grid container direction="column" alignItems="center" justifyContent="center">
+              <img src={item.logo} alt={item.title} style={{ width: '50%', height: '50%' }} />
+              <Typography variant="h5">{item.title}</Typography>
+              <Typography variant="body1">{item.content}</Typography>
+            </Grid>
+          </Paper>
+        ))}
+      </Carousel>
+      <div>
+        <Button onClick={handlePrev}>Previous</Button>
+        <Button onClick={handleNext}>Next</Button>
+      </div>
+    </div>
     </div>
     <Footer />
     </>
--- src/compontes/Top3.jsx
+++ src/compontes/Top3.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { Paper, Button, Typography } from '@mui/material';
-
+import { Paper, Button, Typography, Grid } from '@mui/material';
+import'../App.css';
 const Top3 = () => {
   const items = [
     {
-      title: 'Item 1',
-      content: 'This is the content of Item 1.',
+      title: 'خدمات سريعة',
+      content: 'المستخدم راح يكدر يتواصصل بشكل سريع وبه فريق المبيعات الخاص بكل مجمع ب الاضافة يكدر من خلالنا نكون حلقة تواصل ويه المجمع ',
+      logo: require('./undraw_Speed_test_re_pe1f.png'), // Add the correct image path here
     },
     {
-      title: 'Item 2',
-      content: 'This is the content of Item 2.',
+      title: 'معلومات مباشرة',
+      content: ' المستخدم راح يكدر يعرف معلومات عن المجمعات بسهولة مثل الموقع وخطط الشقق والموقع واراء المستخدمين  ',
+      logo: require('./undraw_World_re_768g.png'), // Add the correct image path here
     },
     {
-      title: 'Item 3',
-      content: 'This is the content of Item 3.',
+      title: 'فريق مبيعات متواجد 24',
+      content: 'تكدرون تتواصلون ويه فريق المبيعات 24 ساعه حيكونون متوفرين بالاضاقة تكدرون تطون افتراحات باضافة المجماعات الجديدة واي خدمات تحبوها ',
+      logo: require('./undraw_Team_up_re_84ok.png'), // Add the correct image path here
     },
   ];
-
+  
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleNext = () => {
@@ -28,18 +31,17 @@ const Top3 = () => {
     setActiveIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
   };
 
-  return (
-    <div>
-      <Carousel
-        autoPlay={false}
-        animation="slide"
-        index={activeIndex}
-        timeout={500}
-      >
+  return (<div className='caro'>
+    <div   style={{alignItems:'center',justifyContent:'center',textAlign:'center',backgroundColor:'red'}}>
+      <h4>مميزات موقع كومبليكس</h4>
+      <Carousel autoPlay={true} animation="slide" index={activeIndex} timeout={5000} style={{alignItems:'center',justifyContent:'center',textAlign:'center'}}>
         {items.map((item, index) => (
-          <Paper key={index}>
-            <Typography variant="h5">{item.title}</Typography>
-            <Typography variant="body1">{item.content}</Typography>
+          <Paper  key={index} style={{ height: '500px' ,width:'60%'}}> 
+            <Grid container direction="column" alignItems="center" justifyContent="center">
+              <img src={item.logo} alt={item.title} style={{ width: '50%', height: '50%' }} />
+              <Typography variant="h5">{item.title}</Typography>
+              <Typography variant="body1">{item.content}</Typography>
+            </Grid>
           </Paper>
         ))}
       </Carousel>
@@ -47,6 +49,7 @@ const Top3 = () => {
         <Button onClick={handlePrev}>Previous</Button>
         <Button onClick={handleNext}>Next</Button>
       </div>
+    </div>
     </div>
   );
 };
